a0637e965f63ecd9d8daafa7f4a4acdd9e2830e6
ymanabe <ymanabe@beaglesoft.net>
2016-04-02 07:34:11 +0900

    ファイルのアップロードサイズの制限を追加した。

--- app/models/picture_store.rb
+++ app/models/picture_store.rb
@@ -1,3 +1,9 @@
+
 class PictureStore < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+
+  validates :picture,
+            :file_size => {
+                :maximum => 1.megabytes.to_i
+            }
 end
--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module CarrierWave
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # lib配下のパスを読み込む
+    config.autoload_paths += %W(#{config.root}/lib)
+
   end
 end
