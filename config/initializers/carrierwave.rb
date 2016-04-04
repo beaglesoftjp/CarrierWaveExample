10e46755bcda5bf7be15405f76707c717a63ee2d
ymanabe <ymanabe@beaglesoft.net>
2016-04-04 11:43:06 +0900

    ファイルアップロードをS3へ行うようにした。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,1 +1,15 @@
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+CarrierWave.configure do |config|
+
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'ap-northeast-1'
+  }
+
+  config.fog_directory = 'carrier-wave' if Rails.env.production?
+  config.fog_directory = 'carrier-wave' if Rails.env.development?
+
+end
